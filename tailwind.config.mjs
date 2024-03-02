01270ec73fe0f2ef109b88c96069721e2ab5a622
CTZDev <carlosteranzavaleta@hotmail.com>
2024-03-02 18:44:38 -0500

    Add layout Header and navbar (Design Mobile)

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "clr-light": "#faf6ec",
+        "clr-dark": "#371a11",
+        "clr-primary": "#d09845",
+        "clr-light-primary": "#e8d0a0",
+        "clr-dark-primary": "#a56429",
+        "clr-black": "#000",
+        "clr-white": "#fff",
+      },
+    },
+  },
+  plugins: [],
+};
