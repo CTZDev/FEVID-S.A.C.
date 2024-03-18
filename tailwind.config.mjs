55b70fe15a4fa79029f7c81a732a97688657c488
CTZDev <carlosteranzavaleta@hotmail.com>
2024-03-17 20:22:27 -0500

    Add scrollReveal in sections Done!

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import animations from "tailwindcss-animated";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -15,4 +18,5 @@ export default {
       },
     },
   },
+  plugins: [animations],
 };
